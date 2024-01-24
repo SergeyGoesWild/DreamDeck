6e333133418552b9f6999eaddb736782760f1e85
espino vincent <tantalas95@gmail.com>
2024-01-24 17:52:24 +0100

    saving end of day 24/01

---
+++ component/footer/FooterComponent.jsx
@@ -0,0 +1,13 @@
+function FooterComponent() {
+
+    return (
+        <div>
+            <h3>Contact us</h3>
+            <h3>Legals</h3>
+            <h3>Join us</h3>
+        </div>
+    )
+
+}
+
+export default FooterComponent
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import { useEffect } from 'react'
+import FooterComponent from '../component/footer/footerComponent';
 import './App.css'
+import './Responsive.css'
 
-function App() {
-
+function App() { 
+   
   const url = "https://api.tcgdex.net/v2/en/cards";
 
-  useEffect(() => {
-    fetch(url)
-      .then(response => {
-        return response.json();
-      })
-      .then(result => {
-        console.log(result);
-      })
-  }, []);
+useEffect(() => {
+  fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(result => {
+      console.log(result);
+    })
+}, []);
 
   return (
     <>
 
+      
+      <footer>
+        <FooterComponent />
+      </footer>
     </>
   )
 }
