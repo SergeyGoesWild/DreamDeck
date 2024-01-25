c247d525ae2fb69c3987a54b554a77c219a92daf
espino vincent <tantalas95@gmail.com>
2024-01-25 07:01:02 +0100

    almost done need to adjust some elements like colors before merge

--- component/footer/FooterComponent.jsx
+++ component/footer/FooterComponent.jsx
@@ -1,7 +1,9 @@
+import './FooterComponent.css'
+
 function FooterComponent() {
 
     return (
-        <div>
+        <div className="FooterContentContainer">
             <h3>Contact us</h3>
             <h3>Legals</h3>
             <h3>Join us</h3>
