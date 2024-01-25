5a65bae8dfeff3a198635a173fbfa55b7e7ffd4b
espino vincent <tantalas95@gmail.com>
2024-01-25 14:17:53 +0100

    linted and prettier , some styling left to do for later

--- component/footer/FooterComponent.jsx
+++ component/footer/FooterComponent.jsx
@@ -1,17 +1,15 @@
-import './FooterComponent.css'
+import "./FooterComponent.css";
 
 function FooterComponent() {
-
-    return (
-        <footer>
-            <div className="footerContentContainer">
-                <h3>Contact us</h3>
-                <h3>Legals</h3>
-                <h3>Join us</h3>
-            </div>
-        </footer>
-    )
-
+  return (
+    <footer>
+      <div className="footerContentContainer">
+        <h3>Contact us</h3>
+        <h3>Legals</h3>
+        <h3>Join us</h3>
+      </div>
+    </footer>
+  );
 }
 
-export default FooterComponent
+export default FooterComponent;
