bb3609e607f47f2ada715a30972a0e981d2c246d
espino vincent <tantalas95@gmail.com>
2024-01-29 15:33:57 +0100

    fix for dev

--- src/components/decksection/DeckSection.jsx
+++ src/components/decksection/DeckSection.jsx
@@ -1,14 +1,12 @@
 const DeckSection = () => {
   return (
-    <div>
-      <div className="deck-section">
-        <div>choose your deck</div>
-        <div className="decklist">
-          <div>deck elec</div>
-          <div>deck</div>
-          <div>deck</div>
-          <div>deck</div>
-        </div>
+    <div className="deck-section">
+      <div>choose your deck</div>
+      <div className="decklist">
+        <div>deck elec</div>
+        <div>deck</div>
+        <div>deck</div>
+        <div>deck</div>
       </div>
     </div>
   );
